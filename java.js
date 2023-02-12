3db74eaddf9e2a815e59d897217682b95031b198
MauroBiazzutti <125225611+MauroBiazzutti@users.noreply.github.com>
2023-02-12 20:54:24 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,33 @@
+document.getElementById ("telefono").addEventListener('click',function() {
+ 
+    
+        document.getElementById ("telefono").innerHTML ="035854621"
+      })
+      document.getElementById ("telefono").addEventListener('dblclick',function(){
+        document.getElementById ("telefono").innerHTML ="clik para ver TELEFONO";
+    
+    })
+       
+    
+    
+ 
+ document.getElementById ("correo").addEventListener('click',function() {
+  
+    document.getElementById ('correo').innerHTML ="jartrabajo1993@gmail.com";
+      
+     
+})
+document.getElementById ("correo").addEventListener('dblclick',function(){
+    document.getElementById ("correo").innerHTML ="clik para ver DIR CORREO    ";
+
+})
+
+
+document.getElementById ("domicilio").addEventListener('click',function() {
+    
+    document.getElementById ('domicilio').innerHTML ="la Tirolesa 1734"
+})
+document.getElementById ("domicilio").addEventListener('dblclick',function(){
+    document.getElementById ("domicilio").innerHTML ="clik para ver DOMICILIO";
+
+})
